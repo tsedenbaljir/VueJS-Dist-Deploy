fe5e6a272702c4470caee0e0352f6fda154b2011
tsedenbaljir <tsedenbaljire@gmail.com>
2020-01-22 13:01:18 +0800

    deploy dist

---
+++ precache-manifest.01fff24be991b4152ea9cc564f8bb5f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3626946fb322c6d372a0",
+    "url": "/css/app.a6bfcc14.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "36c89401c0ff2bdbc690cca1978f6490",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c755773003ccc43900c",
+    "url": "/js/about.1b4c8efc.js"
+  },
+  {
+    "revision": "3626946fb322c6d372a0",
+    "url": "/js/app.c7b05a7b.js"
+  },
+  {
+    "revision": "961d611e0a39ca7b033f",
+    "url": "/js/chunk-vendors.9f1ed624.js"
+  },
+  {
+    "revision": "b8f616f91f5dd688bec399a08da81f34",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
